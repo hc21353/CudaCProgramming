fc991b801fdd999a524e9af92772ac57022a0463
hc21353 <104783527+hc21353@users.noreply.github.com>
2026-03-25 10:47:35 -0400

    Add files via upload

---
+++ lab2-gemm/sgemm_kernel.cu
@@ -0,0 +1,82 @@
+/******************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ******************************************************************************/
+
+#include <stdio.h>
+
+__global__ void mysgemm(int m, int n, int k, const float *A, const float *B, float *C)
+{
+
+    /********************************************************************
+     *
+     * Compute C = A x B
+     *   where A is a (m x k) matrix
+     *   where B is a (k x n) matrix
+     *   where C is a (m x n) matrix
+     *
+     ********************************************************************/
+
+    // INSERT KERNEL CODE HERE
+    unsigned int row = blockIdx.y * blockDim.y + threadIdx.y; //index of row of C
+    unsigned int col = blockIdx.x * blockDim.x + threadIdx.x; //index of col of C
+
+    //access row first (row-major order; coalesced memory access)
+    if (row<m && col<n) {
+        float Cvalue = 0.0f;
+
+        //dot product of row of A an column of B
+        for (int i = 0; i < k; ++i) {
+            Cvalue += A[row*k+i]*B[i*n+col];
+        }
+        C[row*n+col] = Cvalue; //save in matrix C
+    }
+
+}
+
+void basicSgemm(char transa, char transb, int m, int n, int k, float alpha, const float *A, int lda, const float *B, int ldb, float beta, float *C, int ldc, int testRound)
+{
+    if ((transa != 'N') && (transa != 'n'))
+    {
+        printf("unsupported value of 'transa'\n");
+        return;
+    }
+
+    if ((transb != 'N') && (transb != 'n'))
+    {
+        printf("unsupported value of 'transb'\n");
+        return;
+    }
+
+    if ((alpha - 1.0f > 1e-10) || (alpha - 1.0f < -1e-10))
+    {
+        printf("unsupported value of alpha\n");
+        return;
+    }
+
+    if ((beta - 0.0f > 1e-10) || (beta - 0.0f < -1e-10))
+    {
+        printf("unsupported value of beta\n");
+        return;
+    }
+
+    // Initialize thread block and kernel grid dimensions ----------------------
+    // INSERT CODE HERE
+    int BLOCK_SIZE = 16; //use 16*16 thread blocks
+    dim3 dimGrid((n - 1)/BLOCK_SIZE + 1, (m - 1)/BLOCK_SIZE + 1, 1);
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
+
+    for (int i = 0; i < testRound; i++) {
+        // Invoke CUDA kernel --------------------------------------------------
+        // INSERT CODE HERE
+        mysgemm<<<dimGrid, dimBlock>>> (m,n,k,A,B,C);
+
+        
+        cudaDeviceSynchronize();
+    }
+}
+
+
